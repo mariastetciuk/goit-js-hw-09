e13bcb74e017e2fb663663c19e5331656c156280
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-03-17 18:34:58 +0200

    add 1 task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+// let timerId = null;
+
+stopBtnEl.setAttribute('disabled', '');
+
+function onColorSwitcherStart() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtnEl.removeAttribute('disabled');
+  startBtnEl.setAttribute('disabled', '');
+}
+
+function onColorSwitcherStartStop() {
+  clearInterval(timerId);
+  startBtnEl.removeAttribute('disabled');
+  stopBtnEl.setAttribute('disabled', '');
+}
+startBtnEl.addEventListener('click', onColorSwitcherStart);
+stopBtnEl.addEventListener('click', onColorSwitcherStartStop);
